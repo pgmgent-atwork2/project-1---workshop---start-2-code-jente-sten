889f2cae349c9578285fdff3884afa6489e3d635
«pgm-stenhullebroeck» <sten.hullebroeck@student.arteveldehs.be>
2023-04-18 13:33:38 +0200

    added a next button and made it ready for the location roulette

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,51 @@
-let map = L.map('map').setView([51.052618, 3.724709], 12);
+(() => {
+  let map = L.map('map').setView([51.052618, 3.724709], 12);
 
-let marker = L.marker([51.043944, 3.725245]).addTo(map);
+  let marker;
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-let clickedLocation;
-let popup = L.popup();
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  let clickedLocation;
+  let locationDistance;
+  let popup = L.popup();
 
-function onMapClick(e) {
-  let location =
-    Math.round(e.latlng.distanceTo([51.043944, 3.725245])).toString() + 'm';
-  let locationLength = location.toString().length;
-  if (locationLength > 4) {
-    location =
-      location.slice(0, locationLength - 4) +
-      ',' +
-      location.slice(locationLength - 4, locationLength - 3) +
-      'km';
+  function onMapClick(e) {
+    let location =
+      Math.round(e.latlng.distanceTo([51.043944, 3.725245])).toString() + 'm';
+    let locationLength = location.toString().length;
+    if (locationLength > 4) {
+      location =
+        location.slice(0, locationLength - 4) +
+        ',' +
+        location.slice(locationLength - 4, locationLength - 3) +
+        'km';
+    }
+    popup
+      .setLatLng(e.latlng)
+      .setContent('You clicked the map at ' + e.latlng.toString())
+      .openOn(map);
+    clickedLocation = e.latlng;
+    locationDistance = location;
   }
-  popup
-    .setLatLng(e.latlng)
-    .setContent(
-      'You clicked the map at ' +
-        e.latlng.toString() +
-        'which is ' +
-        location +
-        ' away from the target'
-    )
-    .openOn(map);
-  clickedLocation = e.latlng;
-}
+  map.on('click', onMapClick);
 
-addEventListener('click');
+  const submit = document.getElementById('submit');
 
-map.on('click', onMapClick);
+  submit.addEventListener('click', (e) => {
+    return [
+      (marker = L.marker([51.043944, 3.725245]).addTo(map)),
+      setTimeout(() => {
+        window.alert('which is ' + locationDistance + ' away from the target');
+      }, '100'),
+    ];
+  });
+
+  const next = document.getElementById('next');
+
+  next.addEventListener('click', (e) => {
+    return [map.removeLayer(marker)];
+  });
+})();
