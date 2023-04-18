888fddd75dbca96951c3dd8c7efcb1414618af45
«pgm-stenhullebroeck» <sten.hullebroeck@student.arteveldehs.be>
2023-04-18 11:47:28 +0200

    setup for submit

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
+let clickedLocation;
 let popup = L.popup();
 
 function onMapClick(e) {
@@ -31,25 +31,7 @@ function onMapClick(e) {
         ' away from the target'
     )
     .openOn(map);
+  clickedLocation = e.latlng;
 }
 
 map.on('click', onMapClick);
-
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  let R = 6371; // Radius of the earth in km
-  let dLat = deg2rad(lat2 - lat1); // deg2rad below
-  let dLon = deg2rad(lon2 - lon1);
-  let a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  let d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
